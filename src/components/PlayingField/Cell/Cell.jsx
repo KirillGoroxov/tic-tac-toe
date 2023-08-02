37b23938eb88f5ed091a75a0e82686e7228e4cda
KirillGoroxov <106907758+KirillGoroxov@users.noreply.github.com>
2023-08-02 09:33:23 +0400

    sdsd

--- src/components/PlayingField/Cell/Cell.jsx
+++ src/components/PlayingField/Cell/Cell.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
-import c from './Cell.module.scss'
-import { dataContext } from '../../../Context'
+import React, { useContext } from "react";
+import c from "./Cell.module.scss";
+import { dataContext } from "../../../Context";
 
 const Cell = (props) => {
-  const data = useContext(dataContext)
-  const value = props.value
-  // комбинации для победы 
+  const data = useContext(dataContext);
+  const value = props.value;
+  // комбинации для победы
   const winCondition = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,61 +14,61 @@ const Cell = (props) => {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-  ]
-  const click = e => {
+    [2, 4, 6],
+  ];
+  const click = (e) => {
     // Проверяем пустая ли клетка и что никто еще не выиграл
     if (value === null && data.win === false) {
       // в константу получаем данные о том, чей ход
-      const whoseMove = data.whoseMove
+      const whoseMove = data.whoseMove;
       // меняем информацию о том, чей ход в зависимости от того, чей ход был совершен
-      data.setWhoseMove(whoseMove === 'cross' ? 'circle' : 'cross')
+      data.setWhoseMove(whoseMove === "cross" ? "circle" : "cross");
       // получаем id клетки, по которой был совершен клик
-      const id = e.target.id
+      const id = e.target.id;
       // создаем копию массива с информацией об игровом поле
-      const copyDataField = [...props.dataField]
+      const copyDataField = [...props.dataField];
       // меняем значение в массиве по id
-      copyDataField[id] = whoseMove === 'cross' ? 'x' : 'o'
+      copyDataField[id] = whoseMove === "cross" ? "x" : "o";
       // меняем стейт с помощью созданной ранее копии
-      props.setDataField(copyDataField)
+      props.setDataField(copyDataField);
       // проверяем условие победы с помощью специальной функции
-      checkWinner(copyDataField)
-      console.log(localStorage)
+      checkWinner(copyDataField);
     }
-  }
+  };
   // Функция, проверяющая условие победы
   const checkWinner = (board) => {
     // создаем цикл для массива с данными о поле
     for (let i = 0; i < 8; i++) {
-      const [x, y, z] = winCondition[i]
+      const [x, y, z] = winCondition[i];
       if (board[x] && board[x] === board[y] && board[y] === board[z]) {
         // меняем значение победы, чтобы игроки не смогли играть после нее
-        data.setWin(true)
+        data.setWin(true);
         // Прибавляем очки крестику
-        if (board[x] === 'x') {
-          data.setCross(data.cross + 1)
-          localStorage.setItem('x', data.cross)
+        if (board[x] === "x") {
+          data.setCross(data.cross + 1);
+          localStorage.setItem("x", data.cross);
         }
         // Прибавляем очки нолику
-        if (board[x] === 'o') {
-          data.setCircle(data.circle + 1)
-          localStorage.setItem('o', data.circle)
+        if (board[x] === "o") {
+          data.setCircle(data.circle + 1);
+          localStorage.setItem("o", data.circle);
         }
-        return 'win'
+        return "win";
       }
     }
-  }
+  };
   return (
-    <div id={props.id}
+    <div
+      id={props.id}
       className={c.cell}
       style={{
-        color: value === 'x' ?
-          'rgb(207, 112, 112)' : 'rgb(98, 98, 226)'
+        color: value === "x" ? "rgb(108, 160, 180)" : "rgb(137, 100, 196)",
       }}
-      onClick={click}>
+      onClick={click}
+    >
       {value}
     </div>
-  )
-}
+  );
+};
 
-export default Cell
+export default Cell;
--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,20 +1,32 @@
-import { useContext } from 'react'
-import c from './Score.module.scss'
-import { dataContext } from '../../Context'
+import { useContext } from "react";
+import c from "./Score.module.scss";
+import { dataContext } from "../../Context";
 const Score = () => {
-  const data = useContext(dataContext)
-  const styleCross = data.whoseMove === 'cross' ? true : false
-  const styleCircle = data.whoseMove === 'circle' ? true : false
+  const data = useContext(dataContext);
+  const styleCross = data.whoseMove === "cross" ? true : false;
+  const styleCircle = data.whoseMove === "circle" ? true : false;
   return (
     <div className={c.score}>
-      <div className={c.player} style={{ color: 'rgb(207, 112, 112)', borderBottom: styleCross && '4px solid rgb(207, 112, 112)' }}>
+      <div
+        className={c.player}
+        style={{
+          color: "rgb(108, 160, 180)",
+          borderBottom: styleCross && "4px solid rgb(108, 160, 180)",
+        }}
+      >
         x - {data.cross}
       </div>
-      <div className={c.player} style={{ color: 'rgb(98, 98, 226)', borderBottom: styleCircle && '4px solid rgb(98, 98, 226)' }}>
+      <div
+        className={c.player}
+        style={{
+          color: "rgb(137, 100, 196)",
+          borderBottom: styleCircle && "4px solid rgb(137, 100, 196)",
+        }}
+      >
         o - {data.circle}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Score
+export default Score;
